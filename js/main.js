46178ff2896cba8aa0ff7075cd68c0ad89a9580f
Taylor Lacerte <taylorlacerte@Taylors-MBP.attlocal.net>
2019-08-01 18:57:01 -0500

    set up input boxes on board

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const container = document.querySelector(".container");
+/*----- constants -----*/
 
-const board = [
+var board = [
   [null, null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null, null],
@@ -11,15 +11,26 @@ const board = [
   [null, null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null, null],
 ];
+/*----- app's state (variables) -----*/
 
-function render(){
+/*----- cached element references -----*/
+const container = document.querySelector(".container");
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+init();
+
+function init(){
   board.forEach(function(row, ridx){
     row.forEach(function(cell, cidx){
         let newDiv = document.createElement('div');
         newDiv.setAttribute('class', 'cell');
         container.appendChild(newDiv);
+        newDiv.innerHTML = "<input></input";
     })
   })
 }
 
-render();
+
+
