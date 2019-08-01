1d3718b18df1df9f8e74434c4b6a5105b5cdb6ef
Taylor Lacerte <taylorlacerte@Taylors-MBP.attlocal.net>
2019-08-01 18:10:02 -0500

    render the game board

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('hi')
+const container = document.querySelector(".container");
+
+const board = [
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+];
+
+function render(){
+  board.forEach(function(row, ridx){
+    row.forEach(function(cell, cidx){
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'cell');
+        container.appendChild(newDiv);
+    })
+  })
+}
+
+render();
