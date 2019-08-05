68c5a096760cd351e7d3a5758425636e93bd9f9a
Taylor Lacerte <taylorlacerte@Taylors-MacBook-Pro.local>
2019-08-05 15:12:47 -0500

    validate functions for rows columns and boxes

--- js/main.js
+++ js/main.js
@@ -1,47 +1,20 @@
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
-// var sudoku = [
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//     [null, null, null, null, null, null, null, null, null],
-//   ];
-  
+let field = [];
+
 /*----- cached element references -----*/
-// const container = document.querySelector(".container");
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
-// init();
 
-// function init(){
-//   sudoku.forEach(function(row, ridx){
-//     row.forEach(function(cell, cidx){
-//         let newDivs = document.createElement('div');
-//         newDivs.setAttribute('class', 'cell');
-//         container.appendChild(newDivs);
-//         newDivs.innerHTML = "<input></input";
-//     })
-//   })
-// }
-
-
-// write your code below
-let board = [];
-
-function buildBoard() {
+function buildField() {
 	for (let row = 1; row <= 9; row++) {
 		let tr = document.createElement('tr');
 		tr.id = 'row' + row;
-	for (let col = 1; col <= 9; col++) {
-		let td = document.createElement('td');
+		for (let col = 1; col <= 9; col++) {
+			let td = document.createElement('td');
 			td.id = 'col' + col;
 			let input = document.createElement('input');
 			input.type = 'text';
@@ -51,17 +24,108 @@ function buildBoard() {
 			td.appendChild(input);
 			tr.appendChild(td);
 		}
-	document.getElementById('sudoku-table').appendChild(tr);
+		document.getElementById('sudoku-table').appendChild(tr);
 	}
 }
-
-function fillBoard() {
-	board = [];
+function fillField() {
+	field = [];
 	for (let y = 0; y < 9; y++) {
-	    field[y] = [];
-	    for (let x = 0; x < 9; x++) {
-		    field[y][x] = 0;
+		field[y] = [];
+		for (let x = 0; x < 9; x++) {
+			field[y][x] = 0;
 		}
+	}
+	console.log(field);
+}       
+            
+//functions for checking the solution of sudoku
+function validateRow(row) {
+    let contains = [];
+    for (let x = 0; x < row.length; x++) {
+        if (contains.indexOf(row[x]) > -1) {
+            // Already exists
+            return false;
+        } else {
+            // If it's not empty
+            if (row[x] !== 0) {
+                // Not exists
+                contains.push(row[x]);
+            }
+        }
     }
-    console.log(board);
+    return true;
 }
+
+function validateCol(x, rows) {
+    let contains = [];
+    for (let y = 0; y < rows.length; y++) {
+        if (contains.indexOf(rows[y][x]) > -1) {
+            // Already exists
+            return false;
+        } else {
+            // If it's not empty
+            if (rows[y][x] !== 0) {
+                // Not exists
+                contains.push(rows[y][x]);
+            }
+        }
+    }
+    return true;
+}
+
+function validateBlock(blockNum, blockRow) {
+    let block = [];
+    for (let y = 0; y < 3; y++) {
+        let row = [];
+        row[0] = blockRow[y][blockNum * 3];
+        row[1] = blockRow[y][blockNum * 3 + 1];
+        row[2] = blockRow[y][blockNum * 3 + 2];
+        block.push(row);
+    }
+    let contains = [];
+    for (let y = 0; y < block.length; y++) {
+        for (let x = 0; x < block[y].length; x++) {
+            if (contains.indexOf(block[y][x]) > -1) {
+                // Already exists
+                return false;
+            } else {
+                // If it's not empty
+                if (block[y][x] !== 0) {
+                    // Not exists
+                    contains.push(block[y][x]);
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function validateField() {
+    // Validate rows
+    for (let y = 0; y < field.length; y++) {
+        if (!validateRow(field[y])) {
+            return false;
+        }
+    }
+    // Validate cols
+    for (let x = 0; x < field[0].length; x++) {
+        if (!validateCol(x, field)) {
+            return false;
+        }
+    }
+    // Validate blocks
+    for (let blockY = 0; blockY < 3; blockY++) {
+        let blockRow = [
+            field[blockY * 3],
+            field[blockY * 3 + 1],
+            field[blockY * 3 + 2]
+        ];
+        for (let blockX = 0; blockX < 3; blockX++) {
+            if (!validateBlock(blockX, blockRow)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
