65694dbdb2cae7d0e185ecdc20c007f3ff8404d0
Taylor Lacerte <taylorlacerte@Taylors-MacBook-Pro.local>
2019-08-05 14:49:15 -0500

    css for sudoku table

--- js/main.js
+++ js/main.js
@@ -33,3 +33,35 @@
 // }
 
 
+// write your code below
+let board = [];
+
+function buildBoard() {
+	for (let row = 1; row <= 9; row++) {
+		let tr = document.createElement('tr');
+		tr.id = 'row' + row;
+	for (let col = 1; col <= 9; col++) {
+		let td = document.createElement('td');
+			td.id = 'col' + col;
+			let input = document.createElement('input');
+			input.type = 'text';
+			input.maxLength = 1;
+			input.addEventListener('keydown', checkKeyDown);
+			input.addEventListener('input', updateFieldsLeft);
+			td.appendChild(input);
+			tr.appendChild(td);
+		}
+	document.getElementById('sudoku-table').appendChild(tr);
+	}
+}
+
+function fillBoard() {
+	board = [];
+	for (let y = 0; y < 9; y++) {
+	    field[y] = [];
+	    for (let x = 0; x < 9; x++) {
+		    field[y][x] = 0;
+		}
+    }
+    console.log(board);
+}
